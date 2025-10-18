1f9f8a40e3718fb0a4160028fd755520a8eb96c5
Dubcek Leonel <dubcek.leo@gmail.com>
2025-10-18 10:53:15 -0500

    build(pom): agregar dependencias de MapStruct y Lombok
    Se agregaron las dependencias de MapStruct para el mapeo entre DTOs y Lombok para reducir código repetitivo. Esto permitirá simplificar las clases del modelo y mejorar la mantenibilidad del proyecto.

---
+++ src/main/java/org/example/sis_eventos/SisEventosApplication.java
@@ -0,0 +1,13 @@
+package org.example.sis_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sis_eventos/SisEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sis_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
