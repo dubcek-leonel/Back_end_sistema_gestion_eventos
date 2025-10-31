3fb8e72e11171d0b1f81ac70d269bc24ff8ce9db
Dubcek Leonel <dubcek.leo@gmail.com>
2025-10-30 21:48:54 -0500

    ENTITY_USERS TABLA NOMAS

--- src/main/java/org/example/sis_eventos/entity/users.java
+++ src/main/java/org/example/sis_eventos/entity/users.java
@@ -1,13 +1,39 @@
 package org.example.sis_eventos.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class users {
     @Id
     private Long id;
 
+    private String nombre;
     private String email;
-    private 
+
+    private String studentCode;
+    private String fullName;
+    private String role;
+    private String phone;
+    private String career;
+    private String semmester;
+    private String profileImage;
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    private Date modifiedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = new Date();
+        this.modifiedAt = new Date();
+    }
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedAt = new Date();
+    }
 }
