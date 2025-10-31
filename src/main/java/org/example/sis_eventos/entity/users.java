1864ae6881df204216cd726625b3d7a1657ee8b3
Dubcek Leonel <dubcek.leo@gmail.com>
2025-10-30 20:49:31 -0500

    agregue los paquetes etc

---
+++ src/main/java/org/example/sis_eventos/entity/users.java
@@ -0,0 +1,13 @@
+package org.example.sis_eventos.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class users {
+    @Id
+    private Long id;
+
+    private String email;
+    private 
+}
