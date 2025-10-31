6241e17faa13a05570e32675e87b1cb7edde683e
onetofux <mortisedsteam40@gmail.com>
2025-10-30 22:21:12 -0500

    PaymentMapper added

--- src/main/java/org/example/sis_eventos/mapper/PaymentMapper.java
+++ src/main/java/org/example/sis_eventos/mapper/PaymentMapper.java
@@ -1,4 +1,24 @@
 package org.example.sis_eventos.mapper;
 
-public class PaymentMapper {
+import org.example.sis_eventos.dto.PaymentDTO;
+import org.example.sis_eventos.entity.PaymentEntity; // Usando la nomenclatura y paquete 'sis_eventos'
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+
+    PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
+
+    PaymentDTO paymentEntityAPaymentDTO(PaymentEntity paymentEntity);
+
+    List<PaymentDTO> paymentsEntitiesAPaymentDTOs(List<PaymentEntity> paymentEntities);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "fechaCreacion", ignore = true)
+    @Mapping(target = "fechaModificacion", ignore = true)
+    PaymentEntity paymentDTOAPaymentEntity(PaymentDTO paymentDTO);
 }
