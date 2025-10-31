10b5cca3d8c0d2b7c782a845d7f34b59a87c12b4
onetofux <mortisedsteam40@gmail.com>
2025-10-30 22:12:40 -0500

    PaymentDTO added

---
+++ src/main/java/org/example/sis_eventos/dto/PaymentDTO.java
@@ -0,0 +1,26 @@
+package org.example.sis_eventos.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class PaymentDTO {
+
+    private Long id;
+    private Long registrationId;
+    private BigDecimal amount;
+    private String currency;
+    private String paymentMethod;
+    private String paymentStatus;
+    private String transactionReference;
+    private String paymentProofUrl;
+    private Long verifiedBy;
+    private Date verifiedAt;
+    private String notes;
+
+    private Date fechaCreacion;
+    private Date fechaModificacion;
+
+}
